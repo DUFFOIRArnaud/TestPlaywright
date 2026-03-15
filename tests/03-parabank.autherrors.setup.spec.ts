365820eb03d37da25b3de4337aa4470d90b6e937
Arnaud DUFFOIR <146070660+DUFFOIRArnaud@users.noreply.github.com>
2026-03-15 21:53:56 +0100

    Refactoring pour mettre les CT dans un ordre précis Correction de la vérification d'erreurs sur le formulaire de connextion

--- tests/03-parabank.autherrors.setup.spec.ts
+++ tests/03-parabank.autherrors.setup.spec.ts
@@ -2,17 +2,6 @@
 
 import { test, Page, expect } from '@playwright/test';
 import { RegisterPage } from '../pages/RegisterPage';
-import path from 'path';
-import fs from 'fs';
-
-// Avant chaque test, réinitialiser le fichier d'authentification pour garantir un état propre
-test.beforeEach(async ({ page }) => {
-  const authFile = path.join(__dirname, '../playwright/.auth/user.json');
-  if (fs.existsSync(authFile)) {
-    fs.writeFileSync(authFile, '{}', 'utf-8');
-    console.log('Authentication file reset successfully.');
-  }
-});
 
 // Test pour vérifier les messages d'erreur d'authentification
 test('Authentication error messages on Parabank', async ({ page }) => {
